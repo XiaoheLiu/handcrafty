750cb7b0c6fb4dafbf7ebcf9a32c85db80686a17
Athena Liu <sdathena@mail.ubc.ca>
2019-02-12 23:50:58 -0800

    Finish video 9: server-side rendering from styled components

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,5 +1,5 @@
-import styled from "styled-components";
 import Link from "next/link";
+import styled from "styled-components";
 import Router from "next/router";
 import NProgress from "nprogress";
 import Nav from "./Nav";
@@ -40,15 +40,15 @@ const StyledHeader = styled.header`
     grid-template-columns: auto 1fr;
     justify-content: space-between;
     align-items: stretch;
-    @media (max-width: ${props => props.theme.maxWidth}) {
+    @media (max-width: 1300px) {
       grid-template-columns: 1fr;
       justify-content: center;
     }
-    .sub-bar {
-      display: grid;
-      grid-template-columns: 1fr auto;
-      border-bottom: 1px solid ${props => props.theme.lightgrey};
-    }
+  }
+  .sub-bar {
+    display: grid;
+    grid-template-columns: 1fr auto;
+    border-bottom: 1px solid ${props => props.theme.lightgrey};
   }
 `;
 
